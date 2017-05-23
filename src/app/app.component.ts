4c5a22717432d8cde8f95717dd1043aac6439e1c
manisin4 <manisin4@cisco.com>
2017-05-23 13:16:40 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: `
+  <div class="container">
+    <h1>Reactive Forms</h1>
+    <hero-list></hero-list>
+  </div>`
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { NgModule }            from '@angular/core';
+import { BrowserModule }       from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
 
-import { AppComponent } from './app.component';
+import { AppComponent }        from './app.component';
+import { HeroDetailComponent } from './hero-detail.component'; // <-- #1 import component
+import { HeroListComponent }   from './hero-list.component';
+
+import { HeroService }         from './hero.service'; //  <-- #1 import service
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    ReactiveFormsModule // <-- #2 add to Angular module imports
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeroDetailComponent, // <-- #3 declare app component
+    HeroListComponent
+  ],
+  exports: [ // export for the DemoModule
+    AppComponent,
+    HeroDetailComponent,
+    HeroListComponent
+  ],
+  providers: [ HeroService ], // <-- #4 provide HeroService
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
